baa90999ec5d657f3820bbcd2191220cf88bfd8c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-29 15:30:04 -0500

    update

--- index.php
+++ index.php
@@ -137,21 +137,21 @@ require_once "application_queries.php";
 
         <?php if (isset($_GET['update_success'])): ?>
             <div aria-live="polite" aria-atomic="true" class="bg-body-secondary position-relative bd-example-toasts rounded-3">
-              <div class="toast-container p-3" id="toastPlacement">
-                <div class="toast" role="alert" aria-live="polite" aria-atomic="true" data-bs-delay="5000">
-                  <div class="toast-header">
-                    <img src="..." class="rounded me-2" alt="...">
-                    <strong class="me-auto">
-                        <i class="bi bi-circle-fill text-success" style="font-size: 10px; vertical-align: .125rem !important; margin-top: -100px !important;"></i>    
-                        Success
-                    </strong>
-                  </div>
-                  <div class="toast-body">
-                    <strong>Success!</strong> Application updated successfully.
+                <div class="toast-container p-3" id="toastPlacement">
+                  <div class="toast" role="alert" aria-live="polite" aria-atomic="true" data-bs-delay="5000">
+                    <div class="toast-header">
+                      <img src="..." class="rounded me-2" alt="...">
+                      <strong class="me-auto">
+                          <i class="bi bi-circle-fill text-success" style="font-size: 10px; vertical-align: .125rem !important; margin-top: -100px !important;"></i>    
+                          Success
+                      </strong>
+                    </div>
+                    <div class="toast-body">
+                      <strong>Success!</strong> Application updated successfully.
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
             
                 
     <!-- <div class="alert alert-success" role="alert">
