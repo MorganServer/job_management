c0d2f83ac1568f03e85ba5d16871a79b872b1741
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-29 15:20:56 -0500

    update

--- application_queries.php
+++ application_queries.php
@@ -43,12 +43,8 @@ require_once "connection.php";
         if(isset($_POST['job_link'])) { $job_link = mysqli_real_escape_string($conn, $_POST['job_link']); } else { $job_link = ""; }
         if(isset($_POST['job_type'])) { $job_type = mysqli_real_escape_string($conn, $_POST['job_type']); } else { $job_type = ""; }
         if(isset($_POST['notes'])) { $notes = mysqli_real_escape_string($conn, $_POST['notes']); } else { $notes = ""; }
-        $select = "SELECT * FROM jobs WHERE idno = '$idno'";
-        $result = mysqli_query($conn, $select);
-        if (mysqli_num_rows($result) > 0) {
-            $error[] = 'Job already exists!';
-        } else {
-            $query = "UPDATE jobs SET job_title = NULLIF('$job_title',''), company = NULLIF('$company',''), location = NULLIF('$location',''), pay = NULLIF('$pay',''), bonus_pay = NULLIF('$bonus_pay',''), status = NULLIF('$status',''), watchlist = '$watchlist', job_link = NULLIF('$job_link',''), job_type = NULLIF('$job_type',''), interview_set = '$interview_set', notes = NULLIF('$notes','') WHERE job_id = '$id';";
+        
+        $query = "UPDATE jobs SET job_title = NULLIF('$job_title',''), company = NULLIF('$company',''), location = NULLIF('$location',''), pay = NULLIF('$pay',''), bonus_pay = NULLIF('$bonus_pay',''), status = NULLIF('$status',''), watchlist = '$watchlist', job_link = NULLIF('$job_link',''), job_type = NULLIF('$job_type',''), interview_set = '$interview_set', notes = NULLIF('$notes','') WHERE job_id = '$id';";
 
         if (mysqli_query($conn, $query)) {
             // Redirect with success message
