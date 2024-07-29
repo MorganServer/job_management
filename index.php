1988f22f6bf632bbe42b011ab7ea02e580a70417
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-29 15:55:07 -0500

    update

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ require_once "application_queries.php";
 
         <!-- Add Application Modal -->
             <div class="modal fade" id="addApplicationModal" tabindex="-1" aria-labelledby="addApplicationModalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-xl">
+              <div class="modal-dialog modal-xl modal-dialog-scrollable">
                 <div class="modal-content">
                   <div class="modal-header">
                     <h5 class="modal-title" id="addApplicationModalLabel">Add Application</h5>
@@ -42,68 +42,55 @@ require_once "application_queries.php";
                   </div>
                   <div class="modal-body">
                     <form method="POST" action="">
-
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label for="job_title" class="form-label">Job Title</label>
-                                <input type="text" class="form-control" id="job_title" name="job_title">
-                            </div>
-                            <div class="col">
-                                <label for="job_link" class="form-label">Job Link</label>
-                                <input type="text" class="form-control" id="job_link" name="job_link">
-                            </div>
+                        <div class="col">
+                            <label for="job_title" class="form-label">Job Title</label>
+                            <input type="text" class="form-control" id="job_title" name="job_title">
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label for="company" class="form-label">Company</label>
-                                <input type="text" class="form-control" id="company" name="company">
-                            </div>
-                            <div class="col">
-                                <label for="location" class="form-label">Location</label>
-                                <input type="text" class="form-control" id="location" name="location">
-                            </div>
+                        <div class="col">
+                            <label for="job_link" class="form-label">Job Link</label>
+                            <input type="text" class="form-control" id="job_link" name="job_link">
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label for="pay" class="form-label">Pay</label>
-                                <input type="text" class="form-control" id="pay" name="pay">
-                            </div>
-                            <div class="col">
-                                <label for="bonus_pay" class="form-label">Bonus Pay  <span class="text-muted" style="font-size: 10px;">Optional</span></label>
-                                <input type="text" class="form-control" id="bonus_pay" name="bonus_pay">
-                            </div>
-                            <div class="col">
-                                <label class="form-label" for="status">Status</label>
-                                <select class="form-control" name="status">
-                                    <option value="">Please select one...</option>
-                                    <option value="Applied">Applied</option>
-                                    <option value="Interviewed">Interviewed</option>
-                                    <option value="Offered">Offered</option>
-                                    <option value="Rejected">Rejected</option>
-                                </select>
-                            </div>
-                            <div class="col">
-                                <label class="form-label" for="job_type">Job Type</label>
-                                <select class="form-control" name="job_type">
-                                    <option value="">Please select one...</option>
-                                    <option value="Full Time">Full Time</option>
-                                    <option value="Part Time">Part Time</option>
-                                    <option value="Contract">Contract</option>
-                                    <option value="Internship">Internship</option>
-                                    <option value="Temporary">Temporary</option>
-                                </select>
-                            </div>
+                        <div class="col">
+                            <label for="company" class="form-label">Company</label>
+                            <input type="text" class="form-control" id="company" name="company">
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label class="form-label" for="notes">Notes</label>
-                                <textarea class="form-control" name="notes" rows="5"></textarea>
-                            </div>
+                        <div class="col">
+                            <label for="location" class="form-label">Location</label>
+                            <input type="text" class="form-control" id="location" name="location">
                         </div>
-
+                        <div class="col">
+                            <label for="pay" class="form-label">Pay</label>
+                            <input type="text" class="form-control" id="pay" name="pay">
+                        </div>
+                        <div class="col">
+                            <label for="bonus_pay" class="form-label">Bonus Pay  <span class="text-muted" style="font-size: 10px;">Optional</span></label>
+                            <input type="text" class="form-control" id="bonus_pay" name="bonus_pay">
+                        </div>
+                        <div class="col">
+                            <label class="form-label" for="status">Status</label>
+                            <select class="form-control" name="status">
+                                <option value="">Please select one...</option>
+                                <option value="Applied">Applied</option>
+                                <option value="Interviewed">Interviewed</option>
+                                <option value="Offered">Offered</option>
+                                <option value="Rejected">Rejected</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label" for="job_type">Job Type</label>
+                            <select class="form-control" name="job_type">
+                                <option value="">Please select one...</option>
+                                <option value="Full Time">Full Time</option>
+                                <option value="Part Time">Part Time</option>
+                                <option value="Contract">Contract</option>
+                                <option value="Internship">Internship</option>
+                                <option value="Temporary">Temporary</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label" for="notes">Notes</label>
+                            <textarea class="form-control" name="notes" rows="5"></textarea>
+                        </div>
                         <div class="row mb-3 ps-3">
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="1">
