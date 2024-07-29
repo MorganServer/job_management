1f2a8c6d7c9f294bd6d0685cfdaa1ac2852008a1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-29 14:55:31 -0500

    update

--- index.php
+++ index.php
@@ -211,6 +211,21 @@ require_once "application_queries.php";
 </div>
 <!-- end Update Application Modal -->
 
+<!-- Job Details Modal -->
+<div class="modal fade" id="jobDetailsModal" tabindex="-1" aria-labelledby="jobDetailsModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="jobDetailsModalLabel">Job Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="job-details-content">
+                <!-- Job details will be loaded here -->
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php if (isset($_GET['update_success'])): ?>
     <div class="alert alert-success" role="alert">
