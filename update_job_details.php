70f892dc9d184c0b63b349361428857f4f9b21aa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-29 15:52:27 -0500

    update

--- update_job_details.php
+++ update_job_details.php
@@ -106,6 +106,13 @@ if (mysqli_num_rows($result) > 0) {
                     </form>
                 </div>
             </div>
+            <script>
+                tinymce.init({
+                selector: 'textarea',
+                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
+                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap |   removeformat',
+                });
+            </script>
 
     ";
 }
